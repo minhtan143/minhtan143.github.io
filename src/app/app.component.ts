81196bbae288bb5b7a4d9acd30a15f1d63282f41
Tan Pham <minhtantanhiep@gmail.com>
2024-01-02 19:13:04 +0700

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'about-me';
---
+++ src/app/data/models/contact.ts
@@ -0,0 +1,6 @@
+export interface Contact {
+  facebook: string;
+  gmail: string;
+  skype: string;
+  github: string;
+}
