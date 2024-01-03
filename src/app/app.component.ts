5184981750bd3d585af0f756195fcc1f216a8f2b
Tan Pham <minhtantanhiep@gmail.com>
2024-01-03 19:29:14 +0700

    add profile card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HomeLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/layout/home-layout/home-layout.component.ts
+++ src/app/layout/home-layout/home-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.scss'
 })
