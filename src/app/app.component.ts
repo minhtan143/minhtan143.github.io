6712678452a21bd7975eefe12b035448900239cf
Tan Pham <minhtantanhiep@gmail.com>
2024-01-04 23:09:49 +0700

    add tsparticles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
+import { NgParticlesService, NgxParticlesModule } from '@tsparticles/angular';
+import { MoveDirection, OutMode } from '@tsparticles/engine';
+import { loadSlim } from '@tsparticles/slim';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, HomeLayoutComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NgxParticlesModule,
+    FooterComponent,
+    HomeLayoutComponent,
+  ],
+  providers: [NgParticlesService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'about-me';
+
+  particlesOptions = {
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: ['grab', 'attract'],
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: '#0000ff',
+      },
+      links: {
+        enable: true,
+        color: '#008000',
+        distance: 150,
+        opacity: 0.2,
+        width: 0.5,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 100,
+        },
+        value: 100,
+      },
+      opacity: {
+        value: 0.4,
+      },
+      size: {
+        value: { min: 0.6, max: 1 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  constructor(private readonly ngParticlesService: NgParticlesService) {}
+
+  ngOnInit(): void {
+    this.ngParticlesService.init(async (engine) => {
+      await loadSlim(engine, true);
+    });
+  }
 }
